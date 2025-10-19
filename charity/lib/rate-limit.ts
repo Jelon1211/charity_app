daf2b2a8e847d34d8d14d72debcedb13ad96facd
Jelon1211 <jelongames@gmail.com>
2025-10-19 13:05:23 +0200

    rate limit to 1 min

--- charity/lib/rate-limit.ts
+++ charity/lib/rate-limit.ts
@@ -1,6 +1,6 @@
 let lastRequestTime: number | null = null;
 //TODO: move to some consts or wahatever
-const WINDOW_MS = 60 * 60 * 1000;
+const WINDOW_MS = 1 * 1 * 1000;
 
 export function rateLimiter(): void | Response {
   const now = Date.now();
