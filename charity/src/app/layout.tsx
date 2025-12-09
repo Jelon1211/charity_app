527fc3ffb405746ae6f62cd98403b0798381fbb6
Jelon1211 <jelongames@gmail.com>
2025-12-09 19:27:16 +0100

    params fix

--- charity/src/app/layout.tsx
+++ charity/src/app/layout.tsx
@@ -26,10 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
   searchParams?: { [key: string]: string | undefined };
 }) {
-  const bg = searchParams?.bg ? "#333" : "#fff";
-  const text = searchParams?.text ? "#fff" : "#fff";
-  const bgs = searchParams?.bgs ? "#222" : "#222";
-  const colorsecond = searchParams?.colorsecond ? "#444" : "#444";
+  const bg = searchParams?.bg || "#333";
+  const text = searchParams?.text || "#fff";
+  const bgs = searchParams?.bgs || "#222";
+  const colorsecond = searchParams?.colorsecond || "#444";
 
   return (
     <html lang="en">
